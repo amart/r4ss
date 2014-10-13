bbd0af347b4d0fa0a852e14b3be6d64c614f89b5
amart <amart@users.noreply.github.com>
2014-10-13 15:30:28 -0700

    Initial commit

---
+++ R/sim_add_catch.r
@@ -0,0 +1,23 @@
+# Run SS simulations
+# ZTA, 2014-10-13
+# R version 3.1.1, 32-bit
+
+# this function adds a new line for catch in the catch dataframe in the data structure for the SS DAT file
+
+sim_add_catch <- function(dat_struct=NULL,catch_vec=NULL,seas=-1,catch_year=-1)
+{
+    new_dat_struct <- NULL
+
+    if (dat_struct != NULL && catch_vec != NULL && seas > 0 && catch_year > 0)
+    {
+        new_dat_struct <- dat_struct
+
+        if (seas <= dat_struct$nseas && length(catch_vec) == dat_struct$Nfleet)
+        {
+            new_dat_struct$catch   <- rbind(dat_struct$catch,c(catch_vec,catch_year,seas))
+            new_dat_struct$N_catch <- dat_struct$N_catch + 1
+        }
+    }
+
+    return(new_dat_struct)
+}
