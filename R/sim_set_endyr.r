028632dd9fc1df1141d1631194937325bc1d9b6e
amart <amart@users.noreply.github.com>
2014-10-13 15:27:49 -0700

    Initial commit

---
+++ R/sim_set_endyr.r
@@ -0,0 +1,20 @@
+# Run SS simulations
+# ZTA, 2014-10-13
+# R version 3.1.1, 32-bit
+
+# this function sets the variable endyr in the data structure for the SS DAT file
+
+sim_set_endyr <- function(dat_struct=NULL,endyr=-1)
+{
+    if (dat_struct != NULL)
+    {
+        new_dat_struct <- dat_struct
+
+        if (endyr > 0)
+        {
+            new_dat_struct$endyr <- endyr
+        }
+    }
+
+    return(new_dat_struct)
+}
