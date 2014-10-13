ad54cec1d608a88ebf2d45b7952fe5d651e03478
amart <amart@users.noreply.github.com>
2014-10-13 15:57:40 -0700

    Fixed

--- R/sim_set_endyr.r
+++ R/sim_set_endyr.r
@@ -2,20 +2,19 @@
 # ZTA, 2014-10-13
 # R version 3.1.1, 32-bit
 
-# this function adds a new line for catch in the catch dataframe in the data structure for the SS DAT file
+# this function sets the variable endyr in the data structure for the SS DAT file
 
-sim_add_catch <- function(dat_struct=NULL,catch_vec=NULL,seas=-1,catch_year=-1)
+sim_set_endyr <- function(dat_struct=NULL,endyr=-1)
 {
     new_dat_struct <- NULL
 
-    if (!is.null(dat_struct) && !is.null(catch_vec) && seas > 0 && catch_year > 0)
+    if (!is.null(dat_struct))
     {
         new_dat_struct <- dat_struct
 
-        if (seas <= dat_struct$nseas && length(catch_vec) == dat_struct$Nfleet)
+        if (endyr > 0)
         {
-            new_dat_struct$catch   <- rbind(dat_struct$catch,c(catch_vec,catch_year,seas))
-            new_dat_struct$N_catch <- dat_struct$N_catch + 1
+            new_dat_struct$endyr <- endyr
         }
     }
 
