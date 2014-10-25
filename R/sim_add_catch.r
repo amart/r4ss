6ede118a6eaf330ae2e70c8e080a195f29c4d9ef
amart <amart@users.noreply.github.com>
2014-10-25 12:20:15 -0700

    Changes for catch as matrix

--- R/sim_add_catch.r
+++ R/sim_add_catch.r
@@ -1,25 +1,27 @@
-##' this function adds a new line for catch in the catch dataframe in the data structure for the SS DAT file
+##' this function adds new lines for catch in the catch dataframe in the data structure
+##' for the SS DAT file
 ##'
-##' @param dat_struct - DAT structure to be edited
-##' @param catch_vec - catches for each fleet (vector)
-##' @param seas - season that the catches will be applied
+##' @param dat_struct - DAT file data structure to be edited
+##' @param catch_mat - catches for each season (rows) and fleet (cols) (matrix)
 ##' @param catch_year - year that the catches will be applied
 ##' @return edited DAT structure
 ##' @export
 ##'
 
-sim_add_catch <- function(dat_struct=NULL,catch_vec=NULL,seas=-1,catch_year=-1)
+sim_add_catch <- function(dat_struct=NULL,catch_mat=NULL,catch_year=-1)
 {
     new_dat_struct <- NULL
 
-    if (!is.null(dat_struct) && !is.null(catch_vec) && seas > 0 && catch_year > 0)
+    if (!is.null(dat_struct) && !is.null(catch_mat) && dat_struct$nseas > 0 && catch_year > 0)
     {
         new_dat_struct <- dat_struct
 
-        if (seas <= dat_struct$nseas && length(catch_vec) == dat_struct$Nfleet)
+        if (dim(catch_mat)[1] == dat_struct$nseas && dim(catch_mat)[2] == dat_struct$Nfleet)
         {
-            new_dat_struct$catch   <- rbind(dat_struct$catch,c(catch_vec,catch_year,seas))
-            new_dat_struct$N_catch <- dat_struct$N_catch + 1
+            catch_proj_df          <- data.frame(cbind(catch_mat,catch_year,seq(1,dat_struct$nseas)))
+            names(catch_proj_df)   <- names(dat_struct$catch)
+            new_dat_struct$catch   <- rbind(dat_struct$catch,catch_proj_df)
+            new_dat_struct$N_catch <- dat_struct$N_catch + dat_struct$nseas
         }
     }
 
