33a15dd41d544f72a8eda39f9a2188e0e6eb7bc0
amart <zta@uw.edu>
2014-11-26 15:24:36 -0800

    Initial version

---
+++ R/sim_generate_age_comp.r
@@ -0,0 +1,17 @@
+##' this function generates an age composition observation for the fleet in the data structure for the SS DAT file
+##'
+##' @param dat_struct - DAT structure
+##' @param rep_struct - structure returned by SS_output()
+##' @param gen_fleet - fleet number
+##' @param gen_year - year for the generated composition observation
+##' @param apply_error - apply error to the generated observation (NOT IMPLEMENTED)
+##' @return generated age composition vector with sample size
+##' @export
+##'
+
+sim_generate_age_comp <- function(dat_struct=NULL,rep_struct=NULL,gen_fleet=-1,gen_year=-1,apply_error=FALSE)
+{
+    new_age_comp_struct <- NULL
+
+    return(new_age_comp_struct)
+}
